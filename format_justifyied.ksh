23e2fc3f53467495002867677ec352008134429b
Peter Delevoryas <pdelevor@digitalglobe.com>
2018-01-20 18:02:19 -0700

    edit

--- format_justifyied.ksh
+++
@@ -1,2 +0,0 @@
-cat /tmp/envs.txt | sort -t'=' -k2 |  awk -F'=' '{printf( "%25s =  %s\n", $1, $2) }' > envs.sorted.txt
-#keep
--- tunnel_to_dashboard_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#keep
-
-ssh -i ~/.ssh_aws/id_rsa -L 9999:localhost:5678 httpd@jump.east1.gcsdev.com ssh -i httpd_ec2 -L 9999:localhost:9999 centos@ip-10-0-3-74.ec2.internal
-
